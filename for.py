641f185c823ab35c002ff21ae5075369c25416e9
demon-xa <demon-xa@ya.ru>
2020-12-05 12:36:10 +0300

    for, while

---
+++ for.py
@@ -0,0 +1,9 @@
+list = [2,3,4,5,6,7]
+
+for x in list:
+    if(x%2==1 and x>4):
+        print(x)
+        break
+
+#for use when, you know numbers of circle
+#while when, you dont undestand numbers of iterration. While user input number
